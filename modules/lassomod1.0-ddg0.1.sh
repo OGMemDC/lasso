ccf1dabdf25a5d5e651aff13f88ebaa5376c3694
BlackHat O.G., O.G. MemDC, and O.G. BitBlt <heyitsajmail@gmail.com>
2026-01-05 12:54:40 -0800

    Add files via upload

---
+++ modules/lassomod1.0-ddg0.1.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# -*- coding utf-8 -*-
+# vi :set ai;ac;ts=4;bg=dark;backspace=indent,eol,nostop;belloff;columns=80;
+#
+#   --------------------------------------------------------------------------
+#  :                                                                          :
+#  : lasso_ddg.sh -- perform searches using the duckduckgo search engine.     :
+#  :                                                                          :
+#  : While this script is designed to be a module that is loaded by the lasso :
+#  : penetration testing toolkit/framework, it can also function completely   :
+#  : independant of the framework as well.                                    :
+#  :                                                                          :
+#  : Author:	O.G. BitBlt (bitbltog@proton.me)		                      :
+#  : Date:		December 2025                                                 :
+#  :                                                                          :
+#   --------------------------------------------------------------------------
+#
+declare config_file -A
